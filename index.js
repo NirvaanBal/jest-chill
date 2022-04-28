c980d8cf1b2d2cd194a301e68d2309b7a508aa33
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-28 11:42:11 +0530

    Add a function to reverse a string with tests

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ export const capitalize = (string) => {
   if (!string || typeof string !== 'string') return false;
   return string[0].toUpperCase() + string.slice(1);
 };
+
+export const reverse = (string) => {
+  return string.split('').reverse().join('');
+};
--- index.test.js
+++ index.test.js
@@ -1,8 +1,13 @@
-import { capitalize } from './index';
+import { capitalize, reverse } from './index';
 
 test('capitalize string', () => {
   expect(capitalize('nirvaan')).toBe('Nirvaan');
   expect(capitalize('this is a string')).toBe('This is a string');
   expect(capitalize('')).toBeFalsy();
   expect(capitalize(1)).toBeFalsy();
+});
+
+test('reverse string', () => {
+  expect(reverse('nirvaan')).toBe('naavrin');
+  expect(reverse('this is a string')).toBe('gnirts a si siht');
 });
